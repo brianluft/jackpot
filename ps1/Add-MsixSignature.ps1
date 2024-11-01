d1b1157576b00c52be2ca0fd2dfba6a46fbcb636
Brian Luft <brian@electroly.com>
2024-11-01 03:20:17 -0400

    Add-MsixSignature: remove unused bits

--- ps1/Add-MsixSignature.ps1
+++ ps1/Add-MsixSignature.ps1
@@ -9,25 +9,19 @@ if (-not (Test-Path $CertificatePath)) {
 	throw "Certificate not found!"
 }
 
-# Windows SDK 10.0.*.*
+# Windows SDK
 $windowsSdkBaseDir = "C:\Program Files (x86)\Windows Kits\10\Redist"
 $windowsSdkVersion = `
     Get-ChildItem -Path $windowsSdkBaseDir | 
     Where-Object { $_.Name -match '^10\.0\.\d+\.\d+$' } | 
     Sort-Object Name -Descending | 
     Select-Object -First 1 -ExpandProperty Name
-Write-Output "Windows SDK version: $windowsSdkVersion"
-$windowsSdkPlatform = $Platform
-if ($Platform -eq 'arm64') {
-    $windowsSdkPlatform = 'arm'
-}
-$windowsSdkDir = Join-Path -Path $windowsSdkBaseDir -ChildPath "$windowsSdkVersion\ucrt\DLLs\$windowsSdkPlatform"
-if (-not (Test-Path $windowsSdkDir)) {
-    throw "Windows 10 SDK $windowsSdkVersion not found!"
-}
+Write-Output "Windows SDK: $windowsSdkVersion"
 
 $signtool = "C:\Program Files (x86)\Windows Kits\10\bin\$windowsSdkVersion\x64\signtool.exe"
-if (-not (Test-Path $signtool)) {
+if (Test-Path $signtool) {
+    Write-Output "Signtool: $signtool"
+} else {
 	throw "Signtool not found!"
 }
 
