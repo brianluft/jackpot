43c242bc1f0161bc0c41f45480a1b0f5f035ce01
Brian Luft <brian@electroly.com>
2024-11-02 03:38:30 -0400

    Fix format script

--- src/Format-Code.ps1
+++ src/Format-Code.ps1
@@ -2,4 +2,13 @@ $ErrorPreference = 'Stop'
 
 $root = Split-Path -Path $PSScriptRoot -Parent
 $src = "$root\src"
-dotnet tool run dotnet-csharpier "$src"
+Push-Location $src
+try
+{
+    dotnet tool run dotnet-csharpier .
+}
+finally
+{
+    Pop-Location
+}
+
