e873406d1d7d9ee4c153cc3f7a14fe6208cdfce7
Brian Luft <brian@electroly.com>
2024-11-03 00:25:37 -0400

    Single-word search via m3u8 folders

--- src/J.App/M3u8FolderSync.cs
+++ src/J.App/M3u8FolderSync.cs
@@ -1,9 +1,15 @@
-﻿using J.Core;
+﻿using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using J.Core;
 using J.Core.Data;
 
 namespace J.App;
 
-public sealed class M3u8FolderSync(AccountSettingsProvider accountSettingsProvider, LibraryProvider libraryProvider)
+public sealed partial class M3u8FolderSync(
+    AccountSettingsProvider accountSettingsProvider,
+    LibraryProvider libraryProvider
+)
 {
     private static readonly string _filesystemInvalidChars =
         new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
@@ -32,14 +38,18 @@ public sealed class M3u8FolderSync(AccountSettingsProvider accountSettingsProvid
 
             var moviesDir = Path.Combine(dir, "Movies");
             Directory.CreateDirectory(moviesDir);
-            SyncMovies(moviesDir, movies.Values, existingFiles, livingFiles);
+            SyncMovies(moviesDir, movies.Values, livingFiles);
 
             foreach (var tagType in libraryProvider.GetTagTypes())
             {
                 var tagDir = Path.Combine(dir, MakeFilesystemSafe(tagType.PluralName));
                 Directory.CreateDirectory(tagDir);
-                SyncTagType(tagDir, tagType, movies, movieTags, existingFiles, livingFiles);
+                SyncTagType(tagDir, tagType, movies, movieTags, livingFiles);
             }
+
+            var searchDir = Path.Combine(dir, "Search");
+            Directory.CreateDirectory(searchDir);
+            SyncSearch(searchDir, movies.Values, livingFiles);
 
             lock (livingFiles)
             {
@@ -47,22 +57,31 @@ public sealed class M3u8FolderSync(AccountSettingsProvider accountSettingsProvid
                 foreach (var fp in existingFiles)
                     File.Delete(fp);
             }
+
+            DeleteEmptyDirectories(dir);
         }
     }
 
-    private void SyncMovies(
-        string dir,
-        IEnumerable<Movie> movies,
-        HashSet<string> existingFiles,
-        HashSet<string> livingFiles
-    )
+    private void DeleteEmptyDirectories(string dir)
+    {
+        foreach (var subDir in Directory.GetDirectories(dir))
+        {
+            DeleteEmptyDirectories(subDir);
+            if (Directory.GetFileSystemEntries(subDir).Length == 0)
+            {
+                Directory.Delete(subDir);
+            }
+        }
+    }
+
+    private void SyncMovies(string dir, IEnumerable<Movie> movies, HashSet<string> livingFiles)
     {
         Parallel.ForEach(
             movies,
             new ParallelOptions { MaxDegreeOfParallelism = 4 },
             movie =>
             {
-                CopyM3u8(dir, movie, existingFiles, livingFiles);
+                CopyM3u8(dir, movie, livingFiles);
             }
         );
     }
@@ -72,10 +91,11 @@ public sealed class M3u8FolderSync(AccountSettingsProvider accountSettingsProvid
         TagType tagType,
         Dictionary<MovieId, Movie> movies,
         ILookup<TagId, MovieId> movieTags,
-        HashSet<string> existingFiles,
         HashSet<string> livingFiles
     )
     {
+        List<(string TagDir, Movie Movie)> files = [];
+
         foreach (var tag in libraryProvider.GetTags(tagType.Id))
         {
             var tagDir = Path.Combine(dir, MakeFilesystemSafe(tag.Name));
@@ -83,31 +103,117 @@ public sealed class M3u8FolderSync(AccountSettingsProvider accountSettingsProvid
             foreach (var movieId in movieTags[tag.Id])
             {
                 var movie = movies[movieId];
-                CopyM3u8(tagDir, movie, existingFiles, livingFiles);
+                files.Add((tagDir, movie));
             }
         }
+
+        Parallel.ForEach(
+            files,
+            new ParallelOptions { MaxDegreeOfParallelism = 4 },
+            file =>
+            {
+                CopyM3u8(file.TagDir, file.Movie, livingFiles);
+            }
+        );
     }
 
-    private void CopyM3u8(string dir, Movie movie, HashSet<string> existingFiles, HashSet<string> livingFiles)
+    private void CopyM3u8(string dir, Movie movie, HashSet<string> livingFiles)
     {
-        var m3u8FilePath = Path.Combine(
-            dir,
-            MakeFilesystemSafe(Path.GetFileNameWithoutExtension(movie.Filename)) + ".m3u8"
-        );
+        var prefix = Path.Combine(dir, MakeFilesystemSafe(movie.Filename));
+
+        var m3u8FilePath = prefix + ".m3u8";
 
         lock (livingFiles)
         {
-            livingFiles.Add(m3u8FilePath.ToUpperInvariant());
+            // Try: "prefix.m3u8", "prefix 2.m3u8", "prefix 3.m3u8", etc.
+            var i = 1;
+            while (!livingFiles.Add(m3u8FilePath.ToUpperInvariant()))
+            {
+                i++;
+                m3u8FilePath = $"{prefix} {i}.m3u8";
+            }
         }
-
-        if (existingFiles.Contains(m3u8FilePath.ToUpperInvariant()))
-            return;
 
         var m3u8 = libraryProvider.GetM3u8(movie.Id);
 
         File.WriteAllBytes(m3u8FilePath, m3u8);
     }
 
+    private void SyncSearch(string searchDir, IEnumerable<Movie> movies, HashSet<string> livingFiles)
+    {
+        var wordGroups = (
+            from movie in movies
+            from rawWord in WhitespaceRegex().Split(movie.Filename)
+            let word = MakeFilesystemSafe(Strip(rawWord))
+            where word.Length >= 2 && !NumberRegex().IsMatch(word)
+            select (Word: word.ToLowerInvariant(), Movie: movie)
+        )
+            .Distinct()
+            .ToLookup(x => x.Word, x => x.Movie);
+
+        List<(string Dir, Movie Movie)> files = [];
+
+        foreach (var wordGroup in wordGroups)
+        {
+            var firstCharacter = GetFirstCharacter(wordGroup.Key).ToUpperInvariant();
+            var dir = Path.Combine(searchDir, firstCharacter, wordGroup.Key);
+            foreach (var movie in wordGroup)
+            {
+                files.Add((dir, movie));
+            }
+        }
+
+        Parallel.ForEach(
+            files,
+            new ParallelOptions { MaxDegreeOfParallelism = 4 },
+            file =>
+            {
+                Directory.CreateDirectory(file.Dir);
+                CopyM3u8(file.Dir, file.Movie, livingFiles);
+            }
+        );
+    }
+
+    private static string GetFirstCharacter(string str)
+    {
+        var enumerator = StringInfo.GetTextElementEnumerator(str);
+        if (enumerator.MoveNext())
+            return enumerator.GetTextElement();
+
+        throw new ArgumentException("Empty string.", nameof(str));
+    }
+
+    private static string Strip(string str)
+    {
+        if (string.IsNullOrEmpty(str))
+        {
+            return str;
+        }
+
+        StringBuilder sb = new(str.Length);
+
+        foreach (var c in str)
+        {
+            switch (CharUnicodeInfo.GetUnicodeCategory(c))
+            {
+                case UnicodeCategory.UppercaseLetter:
+                case UnicodeCategory.LowercaseLetter:
+                case UnicodeCategory.TitlecaseLetter:
+                case UnicodeCategory.OtherLetter:
+                    sb.Append(c);
+                    break;
+            }
+        }
+
+        return sb.ToString();
+    }
+
     private static string MakeFilesystemSafe(string name) =>
         string.Join("_", name.Split(_filesystemInvalidChars, StringSplitOptions.RemoveEmptyEntries));
+
+    [GeneratedRegex(@"\s+")]
+    private static partial Regex WhitespaceRegex();
+
+    [GeneratedRegex(@"^[\d\-\._]+$")]
+    private static partial Regex NumberRegex();
 }
