8241058101354ff98def2bf195ce1a31bad66fba
Brian Luft <brian@electroly.com>
2026-03-03 17:34:13 -0600

    New icon

---
+++ src/J.App/Resources/generate_icons.bat
@@ -0,0 +1,12 @@
+del App.ico
+pngcrush -brute -ow App.png
+pngcrush -brute -ow App16x16.png
+pngcrush -brute -ow App24x24.png
+pngcrush -brute -ow App32x32.png
+pngcrush -brute -ow App44x44.png
+pngcrush -brute -ow App48x48.png
+pngcrush -brute -ow App150x150.png
+pngcrush -brute -ow App256x256.png
+pngcrush -brute -ow App256x256.png
+pngcrush -brute -ow App310x150.png
+magick App16x16.png App32x32.png App48x48.png App256x256.png App.ico
